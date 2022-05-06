1e1ca5f74297f440dab647dcd90ac0bea9e0e162
Rubens Zimbres <rubenszmm@gmail.com>
2022-05-05 22:13:25 -0300

    Create ResNet_custom_architecture.py

---
+++ TF-Keras/ResNet_custom_architecture.py
@@ -0,0 +1,87 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import tensorflow as tf
+
+_URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
+path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip', origin=_URL, extract=True)
+PATH = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs_filtered')
+
+train_dir = os.path.join(PATH, 'train')
+validation_dir = os.path.join(PATH, 'validation')
+
+BATCH_SIZE = 32
+IMG_SIZE = (160, 160)
+
+train_dataset = tf.keras.utils.image_dataset_from_directory(train_dir,
+                                                            shuffle=True,
+                                                            batch_size=BATCH_SIZE,
+                                                            image_size=IMG_SIZE)
+
+validation_dataset = tf.keras.utils.image_dataset_from_directory(validation_dir,
+                                                                 shuffle=True,
+                                                                 batch_size=BATCH_SIZE,
+                                                                 image_size=IMG_SIZE)
+
+
+class_names = train_dataset.class_names
+
+val_batches = tf.data.experimental.cardinality(validation_dataset)
+test_dataset = validation_dataset.take(val_batches // 5)
+validation_dataset = validation_dataset.skip(val_batches // 5)
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
+validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
+test_dataset = test_dataset.prefetch(buffer_size=AUTOTUNE)
+
+preprocess_input = tf.keras.applications.ResNet50.preprocess_input
+
+# Create the base model from the pre-trained model ResNet50
+
+############### HERE YOU CHANGE TO 9 CHANNELS ###############
+
+channels=3 
+
+IMG_SHAPE = IMG_SIZE + (channels,)
+base_model = tf.keras.applications.ResNet50(input_shape=IMG_SHAPE,
+                                               include_top=True,
+                                               weights='imagenet')
+
+image_batch, label_batch = next(iter(train_dataset))
+feature_batch = base_model(image_batch)
+print(feature_batch.shape)
+
+base_model.summary()
+
+## HERE IS WHERE THE MAGIC HAPPENS
+
+base_model_config = base_model.get_config()
+
+#### HERE THE CHANNELS WILL BE ALTERED TO 9 ####
+
+base_model_config['layers'][0]["config"]["batch_input_shape"]=(None, 160, 160, channels)
+base_model_config['layers'][1]["config"]["padding"]=((channels,channels), (channels,channels))
+
+#######################################################
+
+model=tf.keras.Model().from_config(base_model_config)
+
+model.summary()
+
+inputs = tf.keras.Input(shape=(160, 160, channels))
+x = base_model(inputs, training=False)
+x = tf.keras.layers.GlobalAveragePooling2D()(x)
+x = tf.keras.layers.Dropout(0.2)(x)
+outputs = tf.keras.layers.Dense(1)(x)
+model = tf.keras.Model(inputs, outputs)
+
+base_learning_rate = 0.0001
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(x=train_dataset, epochs=2)
+
+
