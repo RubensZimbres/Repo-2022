3e39b96fefd898ba51be42f6d8a43feaaa4f5c03
Rubens Zimbres <rubenszmm@gmail.com>
2022-05-08 13:16:10 -0300

    Update load_checkpoint.py

--- Wav2Vec-Base/load_checkpoint.py
+++ Wav2Vec-Base/load_checkpoint.py
@@ -92,7 +92,7 @@ with open('vocab.json', 'w') as vocab_file:
 tokenizer = Wav2Vec2CTCTokenizer("./vocab.json", unk_token="[UNK]", pad_token="[PAD]", word_delimiter_token="|")
 
 
-feature_extractor = Wav2Vec2FeatureExtractor(feature_size=1, sampling_rate=16000, padding_value=0.0, do_normalize=True, return_attention_mask=False)
+feature_extractor = Wav2Vec2FeatureExtractor(feature_size=1, sampling_rate=16000, padding_value=0.0, do_normalize=True, return_attention_mask=True)
 
 processor = Wav2Vec2Processor(feature_extractor=feature_extractor, tokenizer=tokenizer)
 
