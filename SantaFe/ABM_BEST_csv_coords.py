89212964f411cb775f864c0b40c663bc13bc3765
Rubens Zimbres <rubenszmm@gmail.com>
2022-06-13 08:48:30 -0300

    Update ABM_BEST_csv_coords.py

--- SantaFe/ABM_BEST_csv_coords.py
+++ SantaFe/ABM_BEST_csv_coords.py
@@ -1,5 +1,4 @@
 from ast import While
-from re import X
 import numpy as np
 import networkx as nx
 import itertools
@@ -17,28 +16,23 @@ import pandas as pd
 import random
 
 np.random.seed(222)
-regra=2159062512564987644819455219116893945895958528152021228705752563807959237655911950549124 #thesis
+regra=30# 2159062512564987644819455219116893945895958528152021228705752563807959237655911950549124 #thesis
 #2159062512564987644819455219116893945895958528152021228705752563807962227809675103689306
-base1=5
+base1=2 #5
 length_clients=280
 length_pros=25
-percentage=int(100-length_clients/length_pros)/100
-degree_of_similarity=0
+degree_of_similarity=2
 environment_noise=0
 
 #datasets: https://chatox.github.io/networks-science-course/practicum/data/
 
+
+
 df=pd.read_csv('/home/theone/other_models/SantaFe/social.csv', sep=',',header=None)
 
-df=df.iloc[0:length_clients+length_clients,:]
+df=df.iloc[0:400,:]
 
 people=np.unique(np.array(df).reshape(1,-1)[0])
-
-
-clients=np.random.randint(5, size=(1,int(percentage*len(people))))[0]
-pros=np.random.randint(5, size=(1,int(len(people)-percentage*len(people))))[0]
-
-all=np.concatenate([clients,pros])
 
 node_attr = dict(zip(people, range(0,len(np.unique(np.array(df).reshape(1,-1)[0])))))
 
@@ -48,21 +42,28 @@ df['from2'] = df['from'].replace(node_attr, regex=True)
 
 df['to2'] = df['to'].replace(node_attr, regex=True)
 
-df2=df.iloc[:len(all),2:]
+df2=df.iloc[:,2:]
 
 edges0=np.array(df2)
+
+clients=np.random.randint(base1, size=(1,int(0.8*len(people))))[0]
+pros=np.random.randint(base1, size=(1,int(len(people)-0.8*len(people))))[0]
+
+all=np.concatenate([clients,pros])
 
 states=np.arange(0,base1)
 dimensions=3
 
-x=np.random.uniform(-1, 0.7, len(all))
-y=np.random.uniform(-1, 0.7, len(all))
+
+x=np.random.uniform(-1, 1, len(all))
+y=x=np.random.uniform(-1, 1, len(all))
 
 coordinates=np.array([x,y]).reshape(-1,2)
 
 individuals=np.arange(0,len(all))
 
 pos=dict(zip(individuals,coordinates))
+
 
 def cellular_automaton(kernel):
     lista=states
@@ -100,37 +101,37 @@ sum_each2=[]
 sum_each3=[]
 sum_each4=[]
 
-part=13
+
 def interact_client(part):
     subject=all[part]
-    most_similar_cli=[i for i in range(0,len(clients)) if np.isclose(subject, clients[i], rtol=1, atol=degree_of_similarity, equal_nan=False)]
-    most_similar_pro=[i for i in range(len(clients),len(all)) if np.isclose(subject, all[i], rtol=1, atol=degree_of_similarity, equal_nan=False)]
+    most_similar_cli=[i for i in range(0,len(clients)) if np.isclose(subject, clients[i], rtol=0.5, atol=degree_of_similarity, equal_nan=False)]
+    most_similar_pro=[i for i in range(len(clients),len(all)) if np.isclose(subject, all[i], rtol=0.5, atol=degree_of_similarity, equal_nan=False)]
+    ## saber com quem esta interagindo
+    
     cc=random.choice(most_similar_cli)
     pp=random.choice(most_similar_pro)
     initial_condition=[clients[cc],all[part],all[pp]]
     clientes.append([part,cc])
     clientes.append([part,pp])
-    #cc2=np.arange(0,len(clients))[cc]
-    #pp2=np.arange(0,len(all))[pp]
-    #coord_update=np.mean([coordinates[part],coordinates[cc2]],axis=0) #,coordinates[pp2]]
-    coord_update=np.mean([coordinates[part],np.mean(coordinates[most_similar_cli])])
+    cc2=np.arange(0,len(clients))[cc]
+    pp2=np.arange(0,len(all))[pp]
+    coord_update=np.mean([coordinates[part],coordinates[cc2],coordinates[pp2]],axis=0)
     pos.update({part: coord_update})
 
     return cellular_automaton(initial_condition)
 
 def interact_pros(part):
     subject2=all[part]
-    most_similar_pros_cli=[i for i in range(0,len(clients)) if np.isclose(subject2, clients[i], rtol=1, atol=degree_of_similarity, equal_nan=False)]
-    most_similar_pros_pro=[i for i in range(len(clients),len(all)) if np.isclose(subject2, all[i], rtol=1, atol=degree_of_similarity, equal_nan=False)]
+    most_similar_pros_cli=[i for i in range(0,len(clients)) if np.isclose(subject2, clients[i], rtol=0.5, atol=degree_of_similarity, equal_nan=False)]
+    most_similar_pros_pro=[i for i in range(len(clients),len(all)) if np.isclose(subject2, all[i], rtol=0.5, atol=degree_of_similarity, equal_nan=False)]
     ccc=random.choice(most_similar_pros_cli)
     ppp=random.choice(most_similar_pros_pro)
     initial_condition=[clients[ccc],all[part],all[ppp]]
     profs.append([part,ppp])
     profs.append([part,ccc])
-    #ccc2=np.arange(0,len(clients))[ccc]
-    #ppp2=np.arange(0,len(all))[ppp]
-    #coord_update=np.mean([coordinates[part],coordinates[ppp2]],axis=0) #,coordinates[ccc2]]
-    coord_update=np.mean([coordinates[part],np.mean(coordinates[most_similar_pros_pro])])
+    ccc2=np.arange(0,len(clients))[ccc]
+    ppp2=np.arange(0,len(all))[ppp]
+    coord_update=np.mean([coordinates[part],coordinates[ccc2],coordinates[ppp2]],axis=0)
     pos.update({part: coord_update})
     return cellular_automaton(initial_condition)
 
@@ -139,13 +140,10 @@ def interact_pros(part):
 
 mean_cli=[]
 mean_pro=[]
-degree_c=[]
-closeness=[]
 
-iterations=80
-
-while iterations>0:
-    iterations=iterations-1
+m=35
+while m>0:
+    m=m-1
     output_client=list(map(lambda x: interact_client(x),range(0,len(clients))))
     output_pros=list(map(lambda x: interact_pros(x),range(len(clients),len(all))))
     all=[int(i*1-environment_noise) for i in np.concatenate([output_client,output_pros]).reshape(1,-1)[0]]
@@ -162,7 +160,7 @@ while iterations>0:
         sum_each2.append(np.transpose(all.count(2)))
         sum_each3.append(np.transpose(all.count(3)))
         sum_each4.append(np.transpose(all.count(4)))
-    if iterations==34:
+    if m==34:
         edges=edges0
     else:
         edges=profs+clientes
@@ -195,35 +193,37 @@ while iterations>0:
 
         for value in list(mapa.values())[length_clients:]:
             cores.append('black')
-    degree_central=nx.degree_centrality(G)
-    degree_c.append(np.mean(list(degree_central.values())))
-    #Degree centrality assigns an importance score based simply on the number of links held by each node.
-    closeness_central=nx.closeness_centrality(G)
-    closeness.append(np.mean(list(closeness_central.values())))
-    #Closeness centrality scores each node based on their ‘closeness’ to all other nodes in the network.
-    fig, ax = plt.subplots(ncols=1, nrows=5,figsize=(11, 17),gridspec_kw={'height_ratios': [3, 1.5,1.5,1.5,1.5]})
+
+    fig, ax = plt.subplots(ncols=1, nrows=3,figsize=(11, 11),gridspec_kw={'height_ratios': [3, 1,1]})
+    #fig.subplots_adjust(bottom=0.5)    
     d = dict(G.degree())
     if base1>=3:
         cmap=mpl.cm.get_cmap('jet_r')
     else:
         cmap=mpl.cm.get_cmap('gray_r')
-    plt.subplot(511)
-    ec = nx.draw_networkx_edges(G, pos, alpha=0.1)
+    #nx.draw(G, with_labels=False, font_weight='light',linewidths=2,width=0.3,node_color=cores,node_size=[(v * 7)+1 for v in degree], cmap=plt.cm.jet)
+    plt.subplot(311)
+    ec = nx.draw_networkx_edges(G, pos, alpha=0.2)
     nc = nx.draw_networkx_nodes(G, pos, nodelist=nodes, node_color=cores, 
-                             node_size=20, cmap=cmap,node_shape='o')
-                             #node_size=[(v.value()*3) for v in degree_central], cmap=cmap,node_shape='o')
+                             node_size=[(v * 6) for v in degree], cmap=cmap,node_shape='H')
+    #cmap = (mpl.colors.ListedColormap(['red', 'orange', 'yellow', 'green']).with_extremes(over='0.25', under='0.75'))
+
+    #bounds = [0, 1, 2, 3, 4]
+    #norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
     norm = mpl.colors.Normalize(vmin=0, vmax=4)
     cbar=plt.colorbar(mpl.cm.ScalarMappable(cmap=cmap, norm=norm))
     cbar.set_label(label='Quality Perception', size='xx-large', weight='bold')
     cbar.ax.tick_params(labelsize='large')
+    #ax.set_ylabel('Quality Perception', fontsize=40) 
+    #plt.colorbar(nc)
     plt.axis('off')
-    plt.subplot(512)
+    plt.subplot(312)
     plt.plot(mean_cli, color='red', label='Clients average perception')
     plt.plot(mean_pro, color='blue', label='Professionals average perception')
-    leg=plt.legend(fontsize=12,loc = "upper left")
+    leg=plt.legend(fontsize=15)
     for line in leg.get_lines():
         line.set_linewidth(3.0)
-    plt.subplot(513)
+    plt.subplot(313)
     if base1==2:
         plt.plot(sum_each, label='Count of zeros', color='red')
         plt.plot(sum_each1, label='Count of 1s', color='orange')
@@ -234,17 +234,7 @@ while iterations>0:
         plt.plot(sum_each3, label='Count of 3s', color='green')
         plt.plot(sum_each4, label='Count of 4s', color='blue')
 
-    leg=plt.legend(fontsize=12,loc = "upper left")
-    for line in leg.get_lines():
-        line.set_linewidth(3.0)
-    plt.subplot(514)
-    plt.plot(degree_c, color='black', label='Degree centrality (average # links)')
-    leg=plt.legend(fontsize=12,loc = "upper left")
-    for line in leg.get_lines():
-        line.set_linewidth(3.0)
-    plt.subplot(515)
-    plt.plot(closeness, color='green', label='Closeness centrality (average closeness)')
-    leg=plt.legend(fontsize=12,loc = "upper left")
+    leg=plt.legend(fontsize=15)
     for line in leg.get_lines():
         line.set_linewidth(3.0)
 
@@ -253,7 +243,7 @@ while iterations>0:
 
 
 fp_in = "/home/theone/Documents/MBA_binary_noise/foo*.png"
-fp_out = "/home/theone/Documents/MBA_github_completo.gif"
+fp_out = "/home/theone/Documents/MBA_github_noise_movie.gif"
 
 img, *imgs = [Image.open(f) for f in sorted(glob.glob(fp_in))]
 img.save(fp=fp_out, format='GIF', append_images=imgs,
